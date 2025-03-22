1799cc062b7113a7e5a2073120aa0ee372df9baa
ONE-THING-9 <amityadu8007@gamil.com>
2025-03-22 23:28:26 +0530

    updated draft page

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuration file for Legal AI application
 const config = {
   // base_url: 'http://localhost:8000',
-  base_url: 'https://184.73.131.8/legal/docs',
+  base_url: 'https://184.73.131.8/legal',
   // Add other configuration parameters here as needed
 }; 
